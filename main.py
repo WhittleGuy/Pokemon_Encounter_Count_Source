4b3236abc79ee8111968997e098e851a1015fecf
Brandon Whittle <brandonwwhittle@gmail.com>
2022-06-17 00:50:22 -0500

    FEAT(): Added auto counter based on all white pixels of encounter sequence

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/usr/local/bin/pipenv
 from pynput import keyboard
-from pynput.keyboard import Listener
+from pynput.keyboard import Listener as KListener
+from pynput.mouse import Listener as MListener
+from PIL import Image, ImageGrab
+import time
 
-# Set text file and step parameters
 FILE = './count.txt'
+SELECTION_AREA_CORNERS = [()]
 STEP_SIZE = 2
-
-# Input all valid key presses and action
 INCREMENT_LIST = ('Key.up', 'Key.right')
 DECREMENT_LIST = ('Key.down', 'Key.left')
 
@@ -22,6 +23,12 @@ class Counter:
         self.file = file
         self.step = step
         self.count = count
+
+    def on_click(self, x, y, button, pressed):
+        if pressed:
+            SELECTION_AREA_CORNERS[0] += (x, y)
+        if len(SELECTION_AREA_CORNERS[0]) >= 4:
+            return False
 
     def keyPress(self, key):
         action = None
@@ -46,11 +53,24 @@ class Counter:
                 text.write(str(self.count))
 
     def run(self):
-        keyboardListener = keyboard.Listener(on_press=self.keyPress)
+        print('Please select two opposite corners of the area you want to watch')
+        with MListener(on_click=self.on_click) as listener:
+            listener.join()
+
+        keyboardListener = KListener(on_press=self.keyPress)
         keyboardListener.start()
-        input()
+
+        while True:
+            image = ImageGrab.grab(bbox=SELECTION_AREA_CORNERS[0])
+            cols = Image.Image.getcolors(image)
+            if cols is not None and len(cols) == 1:
+                if cols[0][1] == (255, 255, 255):
+                    self.change_count('{+}')
+                    time.sleep(5)
+            else:
+                time.sleep(0.125)
 
 
 if __name__ == '__main__':
-    EncounterMachine = Counter(INCREMENT_LIST, DECREMENT_LIST, FILE, STEP_SIZE)
-    EncounterMachine.run()
+    AutoCounter = Counter(INCREMENT_LIST, DECREMENT_LIST, FILE, STEP_SIZE)
+    AutoCounter.run()
